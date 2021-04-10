08b0c54128f23677b65c8880c10a8e595d415f10
Rafał Chłodnicki <rchl2k@gmail.com>
2021-04-10 23:58:08 +0200

    create empty custom.css if not linked

--- addon/rootfs/etc/cont-init.d/tile-board.sh
+++ addon/rootfs/etc/cont-init.d/tile-board.sh
@@ -5,7 +5,13 @@
 # ==============================================================================
 
 CONFIG_FOLDER=$(bashio::config 'configFolder')
+DEST_FOLDER=/var/www/tileboard/
 
 bashio::log.info 'Symlink user configuration files to served folder'
 
-python3 /etc/tileboard/symlink-config.py "/config/$CONFIG_FOLDER" "/var/www/tileboard/"
+python3 /etc/tileboard/symlink-config.py "/config/$CONFIG_FOLDER" "$DEST_FOLDER"
+
+CUSTOM_CSS_PATH="$DEST_FOLDER/styles/custom.css"
+if [ ! -f "$CUSTOM_CSS_PATH" ]; then
+    touch "$CUSTOM_CSS_PATH"
+fi
